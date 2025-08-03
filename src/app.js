8ce242b739d30fd8c46dfa1658bebe438e6ec3eb
abdallahyoussriU3 <abdullahyoussri3435@gmail.com>
2025-08-03 11:53:18 +0300

    Create a new NodeJS app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+module.exports = greet; //Ensure functoin is exportable
+// if run diretly, print output (useful for debugging)
+
+if (require.main === module) {
+  console.log(greet("World"));
+}
